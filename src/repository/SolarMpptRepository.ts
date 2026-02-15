5d4cf70ec3e37c9b80e3851b482ea2a87fefbacf
andold <andold@naver.com>
2026-02-15 10:53:48 +0900

    태양광 콘트롤러 수집정보 백엔드 통합 #1

--- src/repository/SolarMpptRepository.ts
+++ src/repository/SolarMpptRepository.ts
@@ -3,33 +3,33 @@ import axios from "axios";
 // SolarMpptRepository.ts
 class SolarMpptRepository {
 	async search(request?: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post("./solar-mppt/api/search", request)
+		return axios.post("/terran/solar-mppt/search", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async crawl(request?: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post("./solar-mppt/api/crawl", request)
+		return axios.post("/terran/solar-mppt/crawl", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async crawlDetail(request?: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post("./solar-mppt/api/crawl/detail", request)
+		return axios.post("/terran/solar-mppt/crawl/detail", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async crawlClean(request?: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post("./solar-mppt/api/crawl/clean", request)
+		return axios.post("/terran/solar-mppt/crawl/clean", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async backup(request?: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.get("./solar-mppt/api/backup", request)
+		return axios.get("/terran/solar-mppt/backup", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async download(request: any, onSuccess?: any, onError?: any, element?: any) {
 		return axios({
-			url: "./solar-mppt/api/download",
+			url: "/terran/solar-mppt/download",
 			method: "GET",
 			responseType: "blob",
 		}).then(response => {
@@ -44,17 +44,17 @@ class SolarMpptRepository {
 		}).catch(error => onError && onError(request, error, element));
 	}
 	async upload(request: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post(`./solar-mppt/api/upload`, request)
+		return axios.post(`/terran/solar-mppt/upload`, request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async aggregate(request: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post(`./solar-mppt/api/aggregate`, request)
+		return axios.post(`/terran/solar-mppt/aggregate`, request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
 	async test(command: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.get(`./solar-mppt/api/test?command=${command}`)
+		return axios.get(`/terran/solar-mppt/test?command=${command}`)
 			.then(response => onSuccess && onSuccess(command, response.data, element))
 			.catch(error => onError && onError(command, error, element));
 	}
--- src/repository/TsdbRepository.ts
+++ src/repository/TsdbRepository.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // TsdbRepository.ts
 class TsdbRepository {
 	async search(request?: any, onSuccess?: any, onError?: any, element?: any) {
-		return axios.post("./tsdb/api/search", request)
+		return axios.post("/terran/tsdb/search", request)
 			.then(response => onSuccess && onSuccess(request, response.data, element))
 			.catch(error => onError && onError(request, error, element));
 	}
